412ffd6cc93e89e1e9519e74e895a5d21c4fa092
Bowserjr610 <bowserjr6108@gmail.com>
2024-12-07 11:43:07 -0500

    Stuff

--- sechack/menu.gd
+++ sechack/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
